e4eb43faac131509c86dcdac814091a5421da21b
Leonardo Rodrigues <contato@leonardorodrigues.me>
2023-08-23 23:51:52 -0300

    initial commit

---
+++ mysite/first/views.py
@@ -0,0 +1,19 @@
+# from django.shortcuts import render
+# from django.views.generic import TemplateView
+from django.http import HttpResponse
+import datetime
+
+# Create your views here.
+
+
+def get_test(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+# class FirstView(TemplateView):
+#     template_name = 'react-test.html'
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['bundle_js'] = 'react-test/bundle.js'
+#         return context
